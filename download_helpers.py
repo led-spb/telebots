24f73e3efe9aa1b6d7b6667576cdf0c60dcd98f7
Alexey.Ponimash <Alexey.Ponimash@gmail.com>
2018-02-05 16:51:19 +0300

    Fixed bugs

--- download_helpers.py
+++ download_helpers.py
@@ -1,71 +1,139 @@
 # -*- coding: utf-8 -*-
 import re, os.path
-import requests, cookielib
-import json, bencode
+import requests
 import logging
-import urlparse
+import urllib
 import youtube_dl
-
-def create_helper(url, proxy):
-    p = urlparse.urlparse(url)
-    baseurl = "%s://%s/" % (p[0],p.hostname)
-    username = p.username
-    password = p.password
-
-    for cls in DownloadHelper.__subclasses__():
-        if cls.check_url(baseurl):
-            return cls(username, password, proxy)
-    return None
+import lxml.cssselect
+import lxml.html
+import cookielib
+import dateparser
+import datetime
 
 
 class DownloadHelper(object):
-  timeout = 10
-  torrent_path = "."
-  base_url = ""
-  cookies = cookielib.CookieJar()
+    timeout   = 10
+    base_url  = ""
+    cookies   = cookielib.CookieJar()
+    proxy     = None
 
-  def __init__(self, user, passwd, proxy):
-      self.user     = user
-      self.passwd   = passwd
+    def __init__(self, user=None, passwd=None):
+        self.user     = user
+        self.passwd   = passwd
 
-      self.session = requests.Session()
-      self.session.cookies = DownloadHelper.cookies
-      self.session.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
-            'Accept-Charset': 'utf-8'
-      }
+        self.session = requests.Session()
+        self.session.cookies = DownloadHelper.cookies
+        self.session.headers = {
+              'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
+              'Accept-Charset': 'utf-8'
+        }
 
-  @classmethod
-  def check_url(cls, url):
-     return re.search("^%s" % cls.base_url, url) != None
+    @classmethod
+    def check_url(cls, url):
+        return re.search("^%s" % cls.base_url, url) != None
 
-  def download(self, url, target_path=None ):
-     return None
+    def enable_proxy(self):
+        if self.proxy!=None:
+           self.session.proxies = { 'http': self.proxy, 'https': self.proxy }
+
+    def disable_proxy(self):
+        self.session.proxies = {}
+
+    def download(self, url, target_path=None ):
+        req = self.session.get( url )
+        url_hash = filter(None,url.split("/"))[-1]
+        filename = os.path.join( "." if target_path == None else target_path, "%s_%s.torrent" % (self.name, url_hash) )
+        f = open( filename , "wb")
+        f.write( req.content )
+        f.close()
+        req.close()
+        return filename
+
+    def do_search(self, query):
+        return []
 
 
-class NnmClubDownloadHelper(DownloadHelper):
+class ResultItem:
+    __attributes__ = ['id', 'title', 'category', 'link', 'added', 'size']
+
+    def __init__(self, **kwargs ):
+        self.__data__ = kwargs
+        pass
+
+    def __getattr__(self, item):
+        if item in self.__class__.__attributes__:
+            return self.__data__[item] if item in self.__data__ else None
+        return None
+
+
+class RutorHelper(DownloadHelper):
+  base_url   = "http://new-rutor.org"
+  name       = "rutor"
+
+  def do_search(self, query):
+      if query.strip()=='':
+         return []
+      url = u"%s/search/%s/" % (self.base_url, query )
+      logging.debug("url: %s", url)
+      req = self.session.get(url)
+      req.raise_for_status()
+      req.encoding = 'utf-8'
+
+      tree = lxml.html.fromstring( req.text )
+      req.close()
+
+      sel = lxml.cssselect.CSSSelector( u"tr.gai,tr.tum" )
+      results = []
+      for item in sel(tree):
+         results.append( self.parse_result(item) )
+      return results
+
+  def parse_result(self, element):
+      cells = element.cssselect('td')
+
+      href     = cells[1].cssselect('a')[0].attrib['href']
+      link     = self.base_url + href + '/'
+
+      m = re.search('(\d+)$', href)
+      item_id = m.group(1)
+
+      title    = cells[1].cssselect('a')[1].text_content().strip()
+      category = 'rutor'
+      size     = cells[3].text_content().strip()
+
+      date_str = cells[0].text_content().strip()
+      added    = dateparser.parse( date_str, languages=['ru','en'] )# - datetime.datetime(1970, 1, 1) ).total_seconds() )
+      if added != None:
+         added = (added - datetime.datetime(1970, 1, 1) ).total_seconds()
+      else:
+         added = 0
+      return ResultItem(id="_rutor_"+item_id, title=title, category=category, link=link, added=added, size=size)
+
+
+class NnmClubHelper(DownloadHelper):
   base_url   = "https?://(nnm-club\.(name|me|to|tv|lib))|(nnmclub\.to)"
+  name       = "nnmclub"
 
-  def __init__(self, user, passwd, proxy ):
-      DownloadHelper.__init__(self, user, passwd, proxy )
-      self.session.proxies = {'http_proxy': proxy, 'htts_proxy': proxy }
+  def __init__(self, user, passwd ):
+      DownloadHelper.__init__(self, user, passwd )
       self.isAuth = None
       self.base_path = None
       pass
 
-  def check_auth( self, req):
-      #logging.debug(req.text)
+  def _check_auth( self, req):
       status = True if re.search( '<a\s+href="login.php\?logout', req.text, re.I+re.M )!=None else False
       logging.info("Noname-club auth status %s", "ok" if status else "fail" )
       return status
   
-  def login(self):
+  def login(self, req=None):
       if self.isAuth:
          return
       url = '%s/forum/login.php' % self.base_path
-      logging.info("Trying to login")
+      logging.info("Trying to login at %s" % url )
 
-      req = self.session.get( url )
+      if req==None:
+         req = self.session.get( url, stream=False )
+
       m = re.search('<form.*?action="login.php".*?>(.*?)</form>', req.text, re.I+re.M+re.U+re.S)
       form = m.group(1)
       req.close()
@@ -79,33 +147,74 @@ class NnmClubDownloadHelper(DownloadHelper):
           login_data[ m.group(1) ] = m.group(2)
 
       req = self.session.post( url, data = login_data )
-      self.isAuth = self.check_auth( req )
+      req.raise_for_status()
+
+      self.isAuth = self._check_auth( req )
       if not self.isAuth:
          raise Exception("Could not login to tracker.")
       req.close()
       pass
 
+  def do_search(self, query):
+      if query.strip()=='':
+         return []
+      self.enable_proxy()
+      url = "http://nnm-club.me/forum/tracker.php"
+      req = self.session.post(url, data = {
+            'f': u'-1',
+            'nm': query.encode('windows-1251'), 
+            'submit_search': (u'Поиск').encode('windows-1251'),
+      })
+      req.raise_for_status()
+      self.base_url = os.path.dirname( req.url )
+
+      tree = lxml.html.fromstring( req.text )
+      sel = lxml.cssselect.CSSSelector( u"table.forumline.tablesorter tr.prow1,tr.prow2" )
+      results = []
+      for item in sel(tree):
+         results.append( self.parse_result(item) )
+      return results
+
+  def parse_result(self, element):
+      cells = element.cssselect('td')
+
+      href     = cells[2].cssselect('a')[0].attrib['href']
+      m = re.search('\?t=(.*)$', href)
+      item_id = m.group(1)
+
+      title    = cells[2].text_content().strip()
+      category = cells[1].text_content().strip()
+      size     = int(cells[5].cssselect('u')[0].text_content().strip())
+      added    = int(cells[9].cssselect('u')[0].text_content().strip())
+
+      return ResultItem(id="_nnm_"+item_id, title=title, category=category, link=os.path.join(self.base_url,href), added=added, size=size)
+
+
   def download(self, url, target_path=None ):
+      self.enable_proxy()
+      logging.info( "Start downloading %s" % url )
       m = re.search('^https?://[^/]+', url)
       if m==None:
          raise Exception('Invalid URL: "%s"' % url )
       self.base_path = m.group(0)
+      req = None
 
       if self.isAuth==None:
-         self.isAuth = self.check_auth( self.session.get('%s/forum/index.php' % self.base_path) )
+         login_url = '%s/forum/index.php' % self.base_path
+         logging.info( "Check login status at %s" % login_url )
+         logging.info( self.session.proxies )
+
+         req = self.session.get( login_url, stream=False)
+         self.isAuth = self._check_auth( req )
 
       if not self.isAuth:
-         self.login()
+         self.login( req )
 
       m = re.search("viewtopic.php\\?(?:t|p)=(\\d+)", url)
       torrent = m.group(1)
 
       req = self.session.get( url )
       req.raise_for_status()
-
-      #logging.debug( "==== begin page ====" )
-      #logging.debug( req.text )
-      #logging.debug( "==== end page ====" )
 
       torrent_id = ""
       match = re.search( "<a href=\"download.php\\?id=(\\d+)", req.text, re.I+re.M )
@@ -117,11 +226,11 @@ class NnmClubDownloadHelper(DownloadHelper):
          raise Exception("Could not find download id")
 
       # download
-      url = "%s/forum/download.php?id=%s" % (self.base_path, torrent_id )
+      url = u"%s/forum/download.php?id=%s" % (self.base_path, torrent_id )
       req = self.session.get( url )
       req.raise_for_status()
 
-      filename = os.path.join( DownloadHelper.torrent_path if target_path ==None else target_path, "nnm_%s.torrent" % torrent)
+      filename = os.path.join( "." if target_path == None else target_path, "nnm_%s.torrent" % torrent)
 
       f = open( filename , "wb")
       f.write( req.content )
@@ -130,11 +239,14 @@ class NnmClubDownloadHelper(DownloadHelper):
       return filename
 
 
-class YoutubeDownloadHelper(DownloadHelper):
+
+
+class YoutubeHelper(DownloadHelper):
    base_url = 'https?://(www\.youtube\.com|youtu\.be)'
 
    def download(self, url, target_path=None ):
-       ydl_opts = { 'outtmpl': os.path.join(target_path,'%(title)s.%(ext)s') }
+       ydl_opts = { 'outtmpl': unicode(os.path.join(target_path,'%(title)s.%(ext)s')) }
        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
           ydl.download([url])
        return target_path
+      
--- torrentbot.py
+++ torrentbot.py
@@ -2,120 +2,167 @@
 # -*- coding: utf-8 -*-
 import logging
 import argparse
-from telebot import Bot, BotRequestHandler
-import requests
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, MessageHandler, Filters, CallbackQueryHandler
+import json
+import time
 from datetime import datetime
 import re, os.path
+import traceback
 import sys
-import lxml.cssselect
-import lxml.html
+from jinja2 import Environment, Template
+import urlparse
 
 
-class TorrentSearchHandler(BotRequestHandler):
-   def __init__(self, proxy=None):
-       self.logger = logging.getLogger(self.__class__.__name__)
+class ItemRenderer:
+   def __init__(self):
+       self.jinja2 = Environment()
+       self.jinja2.filters['datetimeformat'] = self._datetimeformat
+       self.jinja2.filters['todatetime'] = self._todatetime # {% if item.size | default(0) | number %}{{item.size | default(0) | int | filesizeformat}}{% else %}{{item.size | e}}{% endif %}
+       self.template = self.jinja2.from_string(
+u"""<b>{{item.title|e}}</b>
+Раздел: {{item.category|e}}
+Размер: {% if item.size | int(-1) == -1 %}{{item.size | e}}{% else %}{{ item.size | default(0) | int | filesizeformat }}{% endif %}
+Добавлено: {{ item.added | default(0) | int | todatetime | datetimeformat('%d-%m-%Y') }}
+Скачать: /download{{item.id}}
 
-       self.session = requests.Session()
-       self.session.headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36',
-            'Accept-Charset': 'utf-8'
-       }
-       if proxy!=None:
-         self.session.proxies = { 'http': proxy, 'https': proxy }
-       pass
+""")
 
-   def do_search(self, query):
-       url = "http://nnm-club.me/forum/tracker.php"
-       r = self.session.post(url, data={
-             'nm': query.encode('windows-1251'), 
-             'submit': (u'Поиск').encode('windows-1251'),
-       })
-       self.base_url = os.path.dirname( r.url )
+   def _datetimeformat(self, value, format='%d-%m-%Y %H:%M:%s'):
+       return value.strftime(format)
 
-       tree = lxml.html.fromstring( r.text )
-       sel = lxml.cssselect.CSSSelector( u"table.forumline.tablesorter tr.prow1,tr.prow2" )
-       results = []
-       for item in sel(tree):
-          results.append( self.parse_result(item) )
-       return results
-       pass
-
-   def parse_result(self, element):
-       cells = element.cssselect('td')
-       return {'title': cells[2].text_content().strip(), 
-               'link':  os.path.join( self.base_url, cells[2].cssselect('a')[0].attrib['href'] ),
-               'category': cells[1].text_content().strip(),
-               'size': int(cells[5].cssselect('u')[0].text_content().strip()),
-               'added': int(cells[9].cssselect('u')[0].text_content().strip())
-       }
-
-   def format_item(self, item):
-       message = u"<b>%s</b>\nРаздел: %s\nРазмер: %s\nДобавлено: %s\n%s" % (item['title'], item['category'], self.format_size(item['size']), self.format_time(item['added']), item['link'])
-       buttons = [ {'callback_data': '%s' % item['link'], 'text': 'Download' } ]
-       return { 'text': message, 'markup': { 'inline_keyboard': [ buttons ] }, 'extra': {'parse_mode':'html', 'disable_notification': True, 'disable_web_page_preview': False} }
-
-   def format_time(self, tm ):
-       return datetime.fromtimestamp(tm).strftime("%d.%m.%Y %H:%M")
-   
-   def format_size(self, size):
-       size=int(size)
-       if size>1024*1024*1024:
-          return "%.2f GB" % ( int(size) / (1024.0*1024*1024) )
-       if size>1024*1024:
-          return "%.2f MB" % ( int(size) / (1024.0*1024) )
-       if size>1024:
-          return "%.2f KB" % ( int(size) / (1024.0) )
-       return "%d B" % size
-
-   def cmd_search(self, *query):
-       if len(query)==0:
-          return { 'text': 'Empty query' }
-       results = self.do_search( " ".join(query) )
-       res = [ self.format_item(item) for item in results ]
-       return res if len(res)>0 else None
+   def _todatetime(self, value):
+       return datetime.fromtimestamp( int(value) )
+        
+   def render(self, item):
+       return self.template.render( item = item)
 
 
-class DownloadHandler(BotRequestHandler):
-   def __init__(self, helpers=[], target=".", proxy=None):
-       self.logger  = logging.getLogger(self.__class__.__name__)
+class RequestHandler:
+
+   def __init__(self, updater, target_path, admins, helpers):
+       self.admins  = admins
+       self.target_path = target_path
+       self.updater = updater
        self.helpers = helpers
-       self.proxy   = proxy
-       self.target_path = target
+       self.results = {}
 
-   def getCommand(self, name):
-       return self.cmd_download
+       self.renderer = ItemRenderer()
 
-   def cmd_download(self, full_message):
-       urls = re.findall('https?://\S+', full_message)
-       if len(urls)==0:
+       updater.dispatcher.add_handler( MessageHandler( Filters.text, self.on_message ) )
+       updater.dispatcher.add_handler( CallbackQueryHandler( self.on_callback ) )
+       updater.dispatcher.add_handler( MessageHandler( Filters.command, self.on_command ) )
+       pass
+
+   def on_callback(self, bot, update):
+       if (update.effective_user is None) or (update.effective_user.id not in self.admins):
+          return
+       message_id = update.callback_query.message.message_id
+       user_id = update.effective_user.id
+
+       pos = update.callback_query.data
+       logging.info("On callback %s from %s " % (update.callback_query.data, update.effective_user.id))
+       if user_id in self.results:
+          self.show_results(bot, user_id, message_id, self.results[user_id], int(pos) )
+
+   def on_command(self, bot, update):
+       if (update.effective_user is None) or (update.effective_user.id not in self.admins):
+           return
+
+       message_id = update.effective_message.message_id
+       user_id = update.effective_user.id
+       item_id = re.search( '^\/download(.*)$', update.effective_message.text )
+
+       if user_id in self.results and item_id!=None:
+          item_id = item_id.group(1)
+          for item in self.results[user_id]:
+              if item.id == item_id:
+                 self.on_url( bot, user_id, message_id, item.link )
+                 return
+       pass
+
+   def on_url(self, bot, user_id, message_id, url):
+       logging.info( 'Processing url "%s" from %s' % (url, user_id) )
+       message = bot.sendMessage( chat_id=user_id, text='Downloading...' )
+       message_id = message.message_id
+       try:
+         for helper in self.helpers:
+             if helper.__class__.check_url(url):
+                logging.info( helper.proxy )
+                filename = helper.download( url, self.target_path )
+                bot.editMessageText( chat_id=user_id, message_id=message_id, text='Downloaded to %s' % (filename) )
+                return
+       except:
+         logging.exception('Error while downloading url')
+         error_text = traceback.format_exc()
+         bot.editMessageText( chat_id=user_id, message_id=message_id, text='Sorry, error occured!\n%s' % error_text )
+       pass
+
+   def on_message(self, bot, update):
+       if (update.effective_user is None) or (update.effective_user.id not in self.admins):
+           return
+
+       user_id    = update.effective_user.id
+       message_id = update.effective_message.message_id
+
+       url = re.search("https?://\\S+", update.effective_message.text)
+       if url !=None:
+          self.on_url(bot, user_id, message_id, url.group(0) )
           return
 
-       url = urls[0]
+       logging.info( 'Processing query request "%s" from %s' % (update.effective_message.text, update.effective_user.id) )
+       message = bot.sendMessage( chat_id=user_id, text='Searching...' )
+       message_id = message.message_id
+       query_string = update.effective_message.text
+
+       result = []
+       error_text = None
+
        for helper in self.helpers:
-           if helper.__class__.check_url(url):
-              filename = helper.download( url, self.target_path )
-              return {'text': 'Downloaded to %s' %filename }
-              """
-              try:
-                 try:
-                    filename = helper.download( url, self.target_path )
-                    return {'text': 'Downloaded to %s' %filename }
-                 except Exception as e:
-                    if self.proxy==None:
-                       raise e
+           try:
+              logging.info("Searching %s by %s" % (query_string, helper.__class__.__name__) )
+              data = helper.do_search( query_string )
+              logging.info("Found %d items" % len(data) )
+              result += data
+           except:
+              logging.exception("Error while search")
+              error_text = traceback.format_exc()
+              bot.editMessageText( chat_id=user_id, message_id=message_id, text='Sorry, error occured!\n%s' % error_text )
 
-                    logging.info("Trying to use tor proxy. Cause %s: %s" % ( e.__class__.__name__, str(e) ) )
-                    helper.session.proxies = { 'http': self.proxy, 'https': self.proxy }
-                    helper.isAuth=None
-                    filename = helper.download( url, self.target_path )
+       self.results[ update.effective_user.id ] = result
+       if len(result)==0 and error_text==None:
+          bot.editMessageText( chat_id=user_id, message_id=message_id, text='Sorry, nothing is found' )
+          return
 
-                    return {'text': 'Downloaded to %s' %filename }
-                 finally:
-                    helper.session.proxies = None
-              except Exception as e:
-                 raise e"""
-       return {'text': 'Url "%s" is unknown' %url }
+       self.show_results( bot, user_id, message_id, result )
+       pass
 
+   def show_results(self, bot, chat_id, message_id, results, start=1, page_size=3):
+       data = map( (lambda i: self.renderer.render(results[i]) ), range( start-1, min( start+page_size-1, len(results)) ) )
+       message = u"\n".join(data)
+       logging.debug(message)
+       bot.editMessageText( chat_id=chat_id, message_id=message_id, text=message, parse_mode='HTML' )
+
+       buttons=[]
+       if start>page_size:
+          buttons.append( InlineKeyboardButton(text="Prev %d" % page_size,callback_data = str(start-page_size) ) )
+       if (start+page_size) < len(results):
+          buttons.append( InlineKeyboardButton(text="Next %d" % page_size,callback_data = str(start+page_size) ) )
+       if len(buttons)>0:
+          bot.editMessageReplyMarkup( chat_id=chat_id, message_id=message_id, reply_markup = InlineKeyboardMarkup( inline_keyboard=[buttons] ) )
+       return
+
+
+def create_helper(url):
+    p = urlparse.urlparse(url)
+    baseurl = "%s://%s/" % (p[0],p.hostname)
+    username = p.username
+    password = p.password
+
+    for cls in download_helpers.DownloadHelper.__subclasses__():
+        if cls.check_url(baseurl):
+            return cls(username, password)
+    return None
 
 
 if __name__ == '__main__':
@@ -146,35 +193,24 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    # configure logging
-    bot     = Bot( args.token, args.admins )
-    if args.action=='notify':
-       message = sys.stdin.read()
-       for user in args.admins:
-           bot.send_message( to=user, text=message )
-       exit()
-     
     logging.getLogger("requests").setLevel(logging.DEBUG if args.verbose else logging.INFO)
     logging.getLogger("urllib3").setLevel(logging.DEBUG if args.verbose else logging.INFO)
     logging.basicConfig( format="[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s",  level=logging.DEBUG if args.verbose else logging.INFO, filename=args.logfile )
-    logging.info("Starting torrent bot")
-
-    # Torrent search handler
-    bot.addHandler( TorrentSearchHandler(proxy=args.proxy) )
 
     # Torrent download handler
     helpers = []
     for url in args.helper:
-        helper = download_helpers.create_helper(url, args.proxy)
+        helper = create_helper(url)
         if helper!=None:
+           helper.proxy = args.proxy
            helpers.append(helper)
+           logging.info( helper.proxy )
         else:
            logging.warn( "%s is unknown helper", url)
 
-    # Download handlers
-    bot.addHandler( DownloadHandler(helpers=helpers, target=args.download_dir, proxy=args.proxy) )
+    logging.info("Starting torrent bot, helpers: [%s] " % ",".join( [x.__class__.__name__ for x in helpers] ) )
+    updater = Updater( args.token )
+    handler = RequestHandler( updater, args.download_dir, args.admins, helpers )
 
-    try:
-      bot.loop_forever()
-    finally:
-      pass
+    updater.start_polling( timeout=60 )
+    updater.idle()
